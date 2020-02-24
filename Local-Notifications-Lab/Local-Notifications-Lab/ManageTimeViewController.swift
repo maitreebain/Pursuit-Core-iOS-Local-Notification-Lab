bff7d4dcb16ee2f24d556884eac7564325e9eb3f
maitreebain <maitreebain@gmail.com>
2020-02-23 23:34:55 -0500

    working on date picker

---
+++ Local-Notifications-Lab/Local-Notifications-Lab/ManageTimeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ManageTimeViewController.swift
+//  Local-Notifications-Lab
+//
+//  Created by Maitree Bain on 2/23/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ManageTimeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+}
---
+++ Local-Notifications-Lab/Local-Notifications-Lab/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Local-Notifications-Lab
+//
+//  Created by Maitree Bain on 2/23/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class SettingTimerController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var timerButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textField.delegate = self
+    }
+
+
+}
+
+extension SettingTimerController: UITextFieldDelegate {
+    
+}
